0532fe50fae5f89d80b691cd72a24629a5ba5db9
Jonathan Arias Busto <UO283586@uniovi.es>
2023-06-06 13:10:19 +0200

    todo app finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :tasks
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "home/about"
+  root "tasks#index"
 end
